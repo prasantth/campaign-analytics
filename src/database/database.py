836333c01ae07c24c0046abed08c292be3fd3177
prasantth <prasantth@tanalink.com>
2024-10-14 15:05:54 +0800

    use quote_plus for db password

--- src/database/database.py
+++ src/database/database.py
@@ -5,27 +5,18 @@ from dotenv import load_dotenv
 from urllib.parse import quote_plus
 from src.utils.log import Log
 
-# Load environment variables from .env
 load_dotenv()
 
 try:
-    # Load database connection details from the environment
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
 
-    # Conditionally include the password in the connection string
-    if DB_PASSWORD:
-        DB_PASSWORD = quote_plus(DB_PASSWORD)
-        DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    else:
-        DATABASE_URL = f"postgresql://{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-    print(f"Database URL: {DATABASE_URL}")
-
-    # Create the SQLAlchemy engine and session
+    # Try to create the engine and session
     engine = create_engine(DATABASE_URL)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base = declarative_base()
